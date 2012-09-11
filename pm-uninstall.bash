e0fd650acd6cf34a53f3292344cc95d33a374ae9
satojkovic <satojkovic@gmail.com>
2012-09-12 00:06:40 +0900

    merge short and long opts

--- pm-uninstall.bash
+++ pm-uninstall.bash
@@ -1,35 +1,15 @@
-have pm-uninstall && {
-_short_opts()
-{
-    local cur opts
-    COMPREPLY=()
-    cur="${COMP_WORDS[COMP_CWORD]}"
-    opts="-v -f -c -n -q -h -V -l -L"
-
-    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
-}
-
-_long_opts()
-{
-    local cur opts
-    COMPREPLY=()
-    cur="${COMP_WORDS[COMP_CWORD]}"
-    opts="--verbose --force --checkdeps --no-checkdeps --quiet --help --version --local-lib --local-lib-contained"
-
-    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
-}
-
+have pm-uninstall &&
 _pm_uninstall()
 {
-    local cur
+    local cur opts
+    COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
+    opts="-v -f -c -n -q -h -V -l -L --verbose --force --checkdeps --no-checkdeps --quiet --help --version --local-lib --local-lib-contained"
 
-    if [[ "$cur" == --* ]]; then
-        _long_opts
-    elif [[ "$cur" == -* ]]; then
-        _short_opts
+    if [[ ${cur} == -* ]]; then
+        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+        return 0
     fi
 
-}
+} &&
 complete -F _pm_uninstall pm-uninstall
-}
